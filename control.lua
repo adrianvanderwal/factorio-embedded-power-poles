9e06d5383ec2d8720816850d6c9480a5822e372e
Adrian van der Wal <a.vanderwaljnr@gmail.com>
2019-07-03 20:55:16 +0800

    Dev Commit

--- control.lua
+++ control.lua
@@ -2,21 +2,48 @@ local function init_global()
     global.fepp = {}
     global.fepp.allowed_buildings = {
         ['assembling-machine'] = true,
-        ['lab'] = true
+        ['lab'] = true,
+        ['furnace'] = true
     }
 end
 
--- entity to place pole
+-- entity: the entity at which to to place poles
 local function place_poles(entity)
     --log(entity.name)
     --log(entity.prototype.type)
     --log(global.fepp.allowed_buildings[entity.prototype.type])
+
+
+    -- check if it's a power pole.
+    -- only allow connections to circuit-poles
+
     -- check against list of allowed entities
     if global.fepp.allowed_buildings[entity.prototype.type] then
+
+        -- get entity size, adjust search radius
+        local radius_to_search = math.ceil((math.ceil(entity.bounding_box.right_bottom.x) - math.floor(entity.bounding_box.left_top.x)) * 2)
+        log('Radius: ' .. radius_to_search)
+
         local pos = entity.position
         local new_pos = {math.ceil(entity.bounding_box.right_bottom.x) - 0.5, math.ceil(entity.bounding_box.right_bottom.y) - 0.5}
-        entity.surface.create_entity {name = 'fepp-power-pole', position = pos, force = entity.force}
         entity.surface.create_entity {name = 'fepp-circuit-pole', position = new_pos, force = entity.force}
+        entity.surface.create_entity {name = 'fepp-power-pole', position = pos, force = entity.force}
+
+        local power_pole = entity.surface.find_entities_filtered({name = {'fepp-power-pole'}, area = entity.bounding_box, limit = 1, force = entity.force})[1]
+        local circuit_pole = entity.surface.find_entities_filtered({name = {'fepp-circuit-pole'}, area = entity.bounding_box, limit = 1, force = entity.force})[1]
+
+        power_pole.disconnect_neighbour()
+        circuit_pole.disconnect_neighbour()
+        power_pole.connect_neighbour(circuit_pole)
+
+        -- get all circuit poles in radius
+        -- connect to closest (?)
+
+        local neighbour_circuit_poles = entity.surface.find_entities_filtered({name = {'fepp-circuit-pole'}, position = entity.position, radius = radius_to_search, force = entity.force})
+        for k, v in pairs (neighbour_circuit_poles) do
+            -- statements
+            circuit_pole.connect_neighbour(v)
+        end
     end
 end
 
--- prototypes/electric-poles.lua
+++ prototypes/electric-poles.lua
@@ -4,9 +4,8 @@ if not data.raw['electric-pole']['medium-electric-pole'] then
 end
 
 local pole = table.deepcopy(data.raw['electric-pole']['medium-electric-pole'])
-local pole2 = table.deepcopy(data.raw['electric-pole']['medium-electric-pole'])
 
---log(serpent.block(pole))
+log(serpent.block(pole))
 
 pole.minable = nil
 pole.flags = {
@@ -25,29 +24,40 @@ circuit_pole.supply_area_distance = 0
 circuit_pole.selection_priority = 255
 circuit_pole.draw_copper_wires = true
 
+-- actual power pole at centre of machine
 power_pole.name = 'fepp-power-pole'
 power_pole.selection_box = {{0, 0}, {0, 0}}
-power_pole.draw_copper_wires = false
+--power_pole.draw_copper_wires = false
+power_pole.draw_circuit_wires = false
+pole.maximum_wire_distance = 0
 power_pole.supply_area_distance = 3.5
 power_pole.selection_priority = 255
-
-
-
-log(serpent.block(circuit_pole))
-log(serpent.block(power_pole))
+power_pole.connection_points = {
+    {
+        wire = {
+            copper = {0, 0},
+            red = {-0.375, -0.375},
+            green = {0.375, 0.375}
+        },
+        shadow = {
+            copper = {0, 0},
+            red = {-0.375, -0.375},
+            green = {0.375, 0.375}
+        }
+    }
+}
+power_pole.pictures = {
+    filename = '__core__/graphics/empty.png',
+    priority = 'extra-high',
+    width = 1,
+    height = 1,
+    direction_count = 1
+}
+log(serpent.block({circuit_pole, power_pole}))
 
 data:extend(
     {
-        circuit_pole, power_pole
+        circuit_pole,
+        power_pole
     }
 )
-
--- create embedded poles
-local sizes = {
-    [3] = true, -- Assembling Machine (Chem Plant, Centrifuge), Electric Furnace, Lab, Beacon (?)
-    [5] = true, -- Refinery
-    [9] = true -- Rocket Silo
-}
-for k, v in pairs(sizes) do
-    -- statements
-end
